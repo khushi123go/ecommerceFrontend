41ea9323b5d935366d3499c6f040a600d3499551
khushi123go <tiwarikhushi907@gmail.com>
2023-12-18 18:14:56 +0530

    part 2 : made login signup and routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,40 @@ import logo from './logo.svg';
 import  Counter  from './features/counter/Counter';
 import './App.css';
 import ProductList from './features/product-list/ProductList';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 import Home from './pages/Home';
+import React from "react";
+import {createRoot} from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link
+} from "react-router-dom";
+import "./index.css";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home></Home>),
+  },
+  {
+    path: "/login",
+    element: (<LoginPage></LoginPage>),
+  },
+  {
+    path: "/signup",
+    element: (<SignUpPage></SignUpPage>),
+  },
+]);
+
 
 function App() {
   return (
     <div className="App">
-    <Home></Home>
+    <RouterProvider router={router}/>
     </div>
   );
 }
