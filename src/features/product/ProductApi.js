2c940e926a45d8eeb556b0f8db32ddf14898a7bc
khushi123go <tiwarikhushi907@gmail.com>
2023-12-20 00:44:39 +0530

    part:5 understood json-server api making and used it

--- src/features/product/ProductApi.js
+++ src/features/product/ProductApi.js
@@ -1,7 +1,24 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
+export function fetchAllProducts() {
   return new Promise(async (resolve) =>{
-    const response = await fetch('http://localhost:8080')
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products') 
+    const data = await response.json()
+    resolve({data})
+  }
+  );
+}
+export function fetchProductsByFilters(filter) {
+
+  //filter opject= {"category":"smartphone"}
+  //TODO: on server we will support mulyivalues
+  let queryString='';
+  for(let key in filter){
+    queryString += `${key}=${filter[key]}&`
+  }
+  return new Promise(async (resolve) =>{
+    //TODO: we will not hard-code server URL here
+    const response = await fetch('http://localhost:8080/products?'+queryString) 
     const data = await response.json()
     resolve({data})
   }
