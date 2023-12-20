2082bf0667877a9bbf5319ff250a4197efeab721
khushi123go <tiwarikhushi907@gmail.com>
2023-12-20 11:16:01 +0530

    part7: filter and sort changes made

--- src/features/product/ProductApi.js
+++ src/features/product/ProductApi.js
@@ -8,13 +8,20 @@ export function fetchAllProducts() {
   }
   );
 }
-export function fetchProductsByFilters(filter) {
+export function fetchProductsByFilters(filter,sort) {
 
   //filter opject= {"category":"smartphone"}
   //TODO: on server we will support mulyivalues
   let queryString='';
   for(let key in filter){
-    queryString += `${key}=${filter[key]}&`
+    const categoryValues = filter[key];
+    if(categoryValues.length){
+      const lastCategoryValue = categoryValues[categoryValues.length-1]
+      queryString += `${key}=${lastCategoryValue}&`
+    }
+  }
+  for(let key in sort){
+     queryString += `${key}=${sort[key]}&`
   }
   return new Promise(async (resolve) =>{
     //TODO: we will not hard-code server URL here
--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -90,25 +90,33 @@ export default function ProductList() {
   const dispatch = useDispatch();
   const products = useSelector(selectAllProducts);
   const [filter, setFilter] = useState({});
-  
+  const [sort,setSort] = useState({})
   const handleFilter = (e,section,option)=>{
-    const newFilter = {...filter,[section.id]:option.value}
+    const newFilter = {...filter}
+      if(e.target.checked){
+        if(newFilter[section.id]){
+            newFilter[section.id].push(option.value);
+        }else{
+          newFilter[section.id] = [option.value]
+        }
+      
+      }else{
+        const index = newFilter[section.id].findIndex(el=>el===option.value)
+        newFilter[section.id].splice(index,1);
+      }
+      console.log({newFilter});
+
     setFilter(newFilter)
-    dispatch(fetchProductsByFiltersAsync(newFilter))
-    
-   
   } 
   const handleSort = (e,option)=>{
-    const newFilter = {...filter,_sort:option.sort,_order:option.order}
-    setFilter(newFilter)
-    dispatch(fetchProductsByFiltersAsync(newFilter))
-    
-    
-  } 
+    const sort = {_sort:option.sort,_order:option.order}
+    console.log({sort});
+    setSort(sort)
+  }    
   
   useEffect(() => {
-    dispatch(fetchAllProductsAsync());
-  }, [dispatch]);
+    dispatch(fetchProductsByFiltersAsync({filter,sort}))
+  }, [dispatch,filter,sort]);
   // console.log(products)
 
      
